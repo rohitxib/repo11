23be750a157858fac37b1c027005d400a7a38a05
Aplite_Mac <>
2022-11-15 17:36:21 +0530

    added vc2

---
+++ Demogit/Demogit/ViewController2.h
@@ -0,0 +1,16 @@
+//
+//  ViewController2.h
+//  Demogit
+//
+//  Created by Aplite_Mac on 15/11/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewController2 : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Demogit/Demogit/ViewController2.m
@@ -0,0 +1,31 @@
+//
+//  ViewController2.m
+//  Demogit
+//
+//  Created by Aplite_Mac on 15/11/22.
+//
+
+#import "ViewController2.h"
+
+@interface ViewController2 ()
+
+@end
+
+@implementation ViewController2
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"it is second vc");
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
